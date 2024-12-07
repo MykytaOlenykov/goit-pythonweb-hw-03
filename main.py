9ac43993485270ad9d7934124701512f98d0f6f5
Mykyta <nikita2001395@gmail.com>
2024-12-07 23:55:43 +0200

    add: route for read messages

--- main.py
+++ main.py
@@ -1,78 +1,13 @@
-import urllib.parse
-import mimetypes
-import pathlib
-
 from http import HTTPStatus
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer
 
-from jinja2 import Environment, FileSystemLoader
 
-from server import HttpResponse
+from server import HttpResponse, HttpRequestHandler
 from forms import MessageForm
 from repositories import messages_repository, courses_repository
 
 
-ROOT_PATH = pathlib.Path().joinpath()
-TEMPLATES_PATH = ROOT_PATH / "templates"
-
 nav_menu = {"/": "Home", "/message": "Send message"}
-
-
-class HttpRequestHandler(BaseHTTPRequestHandler):
-    env = Environment(loader=FileSystemLoader("templates"))
-
-    routes = {}
-
-    @classmethod
-    def register_route(cls, path, method):
-        def wrapper(func):
-            cls.routes[(path, method)] = func
-            return func
-
-        return wrapper
-
-    def do_GET(self):
-        pr_url = urllib.parse.urlparse(self.path)
-
-        if pr_url.path.startswith("/static/"):
-            self.send_static()
-        else:
-            handler = self.routes.get((pr_url.path, "GET"))
-            if handler:
-                handler(self)
-            else:
-                self.handle_404()
-
-    def do_POST(self):
-        pr_url = urllib.parse.urlparse(self.path)
-        handler = self.routes.get((pr_url.path, "POST"))
-        if handler:
-            data = self.rfile.read(int(self.headers["Content-Length"]))
-            data_parse = urllib.parse.unquote_plus(data.decode())
-            data_dict = dict(el.split("=") for el in data_parse.split("&"))
-            handler(self, data_dict)
-        else:
-            self.handle_404()
-
-    def render_template(self, template_name, context=None, status=HTTPStatus.OK):
-        template = self.env.get_template(template_name)
-        content = template.render({"request": self, **(context or {})})
-
-        response = HttpResponse(content, status)
-        response.send(self)
-
-    def send_static(self):
-        self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
-        self.send_header("Content-type", mt[0] or "text/plain")
-        self.end_headers()
-
-        with open(ROOT_PATH / f".{self.path}", "rb") as file:
-            self.wfile.write(file.read())
-
-    def handle_404(self):
-        response = HttpResponse("Not found", status=HTTPStatus.NOT_FOUND)
-        response.send(self)
 
 
 def handle_404(request: HttpRequestHandler):
@@ -111,6 +46,13 @@ def create_message(request: HttpRequestHandler, body):
             "form": form,
         }
         request.render_template("message.html", context, status=HTTPStatus.BAD_REQUEST)
+
+
+@HttpRequestHandler.register_route("/read", "GET")
+def messages_list(request: HttpRequestHandler):
+    messages = messages_repository.get_all()
+    context = {"nav_menu": nav_menu, "messages": messages}
+    request.render_template("messages-list.html", context)
 
 
 def run(server_class=HTTPServer, handler_class=HttpRequestHandler):
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
+import urllib.parse
+import mimetypes
+import pathlib
+
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader
+
+
+ROOT_PATH = pathlib.Path().joinpath()
+TEMPLATES_PATH = ROOT_PATH / "templates"
 
 
 class HttpResponse:
@@ -20,3 +30,60 @@ class HttpResponse:
 
         request.end_headers()
         request.wfile.write(self.content)
+
+
+class HttpRequestHandler(BaseHTTPRequestHandler):
+    env = Environment(loader=FileSystemLoader("templates"))
+
+    routes = {}
+
+    @classmethod
+    def register_route(cls, path, method):
+        def wrapper(func):
+            cls.routes[(path, method)] = func
+            return func
+
+        return wrapper
+
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+
+        if pr_url.path.startswith("/static/"):
+            self.send_static()
+        else:
+            handler = self.routes.get((pr_url.path, "GET"))
+            if handler:
+                handler(self)
+            else:
+                self.handle_404()
+
+    def do_POST(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        handler = self.routes.get((pr_url.path, "POST"))
+        if handler:
+            data = self.rfile.read(int(self.headers["Content-Length"]))
+            data_parse = urllib.parse.unquote_plus(data.decode())
+            data_dict = dict(el.split("=") for el in data_parse.split("&"))
+            handler(self, data_dict)
+        else:
+            self.handle_404()
+
+    def render_template(self, template_name, context=None, status=HTTPStatus.OK):
+        template = self.env.get_template(template_name)
+        content = template.render({"request": self, **(context or {})})
+
+        response = HttpResponse(content, status)
+        response.send(self)
+
+    def send_static(self):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        self.send_header("Content-type", mt[0] or "text/plain")
+        self.end_headers()
+
+        with open(ROOT_PATH / f".{self.path}", "rb") as file:
+            self.wfile.write(file.read())
+
+    def handle_404(self):
+        response = HttpResponse("Not found", status=HTTPStatus.NOT_FOUND)
+        response.send(self)
